6860592b89740696f602525f45e04e032c49ef3b
Julien Calixte <juliencalixte@gmail.com>
2021-04-11 13:55:00 +0200

    :sparkles: (update path) update link paths in markdown files

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,58 @@
 import * as vscode from "vscode"
+import { resolve, relative } from "path"
+const markdownLinkExtractor = require("markdown-link-extractor")
+
+const updateLinkPath = async (
+  files: readonly {
+    oldUri: vscode.Uri
+    newUri: vscode.Uri
+  }[]
+) => {
+  const notes = await vscode.workspace.findFiles("**/*.md")
+
+  for (const note of notes) {
+    const noteContentBuffer = await vscode.workspace.fs.readFile(note)
+
+    const initialNoteContent = noteContentBuffer.toString()
+    let noteContent = initialNoteContent
+
+    for (const fileRenamed of files) {
+      if (!fileRenamed.newUri.fsPath.endsWith("md")) {
+        continue
+      }
+
+      const noteFolderPath = resolve(note.path, "../")
+
+      const newRelativePath = relative(noteFolderPath, fileRenamed.newUri.path)
+
+      const parsedNewRelativePath = newRelativePath.startsWith("../")
+        ? newRelativePath
+        : newRelativePath.startsWith("./")
+        ? newRelativePath
+        : `./${newRelativePath}`
+
+      const links: string[] = markdownLinkExtractor(noteContent)
+
+      for (const link of links) {
+        const absoluteURI = resolve(noteFolderPath, link)
+
+        if (absoluteURI === fileRenamed.oldUri.path) {
+          noteContent = noteContent.replace(
+            RegExp(`\(${link}\)`),
+            parsedNewRelativePath
+          )
+        }
+      }
+    }
+
+    if (initialNoteContent !== noteContent) {
+      vscode.workspace.fs.writeFile(
+        note,
+        new Uint8Array(Buffer.from(noteContent))
+      )
+    }
+  }
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -6,7 +60,28 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
+  const disposeRenameFiles = vscode.workspace.onDidRenameFiles(
+    async (fileRenameEvent) => {
+      if (
+        !fileRenameEvent.files.some((file) =>
+          file.newUri.fsPath.endsWith(".md")
+        )
+      ) {
+        return
+      }
+
+      vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          cancellable: false,
+          title: "Updating link path...",
+        },
+        () => updateLinkPath(fileRenameEvent.files)
+      )
+    }
+  )
+
+  const commandDisposable = vscode.commands.registerCommand(
     "lite-note.newFleetingNote",
     async () => {
       try {
@@ -37,12 +112,12 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
         const wsPath = vscode.workspace?.workspaceFolders?.[0].uri.fsPath
-        const filePath = vscode.Uri.file(`${wsPath}/_inbox/${today}.md`)
 
         if (!wsPath) {
           return
         }
 
+        const filePath = vscode.Uri.file(`${wsPath}/_inbox/${today}.md`)
         const worspaceEdit = new vscode.WorkspaceEdit()
         worspaceEdit.createFile(filePath, { ignoreIfExists: true })
         worspaceEdit.insert(
@@ -58,7 +133,7 @@ export function activate(context: vscode.ExtensionContext) {
     }
   )
 
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(...[commandDisposable, disposeRenameFiles])
 }
 
 // this method is called when your extension is deactivated
